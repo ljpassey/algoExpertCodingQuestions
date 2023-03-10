c2d2b068d5924cb88cfe145f095c38386453a618
Luke Passey <ljpassey@gmail.com>
2023-03-09 21:51:27 -0700

    This commit includes updated questions.

--- moveElementToEnd.js
+++ moveElementToEnd.js
@@ -1,13 +1,24 @@
 function moveElementToEnd(array, toMove) {
   // Write your code here
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === toMove) {
-      array.splice(i, 1)
-      array.push(toMove);
+    //declare ansArr
+    let ansArr = [];
+    //iterate through array
+    for (i = 0; i < array.length; i++) {
+        //if array[i] === toMove
+        if (array[i] === toMove) {
+            //push array[i] to ansArr
+            ansArr.push(array[i]);
+        }
     }
-  }
-  return array;
+    //iterate through array
+    for (i = 0; i < array.length; i++) {
+        //if array[i] !== toMove
+        if (array[i] !== toMove) {
+            //push array[i] to ansArr
+            ansArr.push(array[i]);
+        }
+    }
+    return ansArr;
 }
 
 console.log(moveElementToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2));
