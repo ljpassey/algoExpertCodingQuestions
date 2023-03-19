489d65de9a14ad6a8cd98417c9b4accaf5f3571d
Luke Passey <ljpassey@gmail.com>
2023-03-19 12:14:47 -0600

    Commit update

---
+++ nodeDepths.js
@@ -0,0 +1,12 @@
+function nodeDepths(root) {
+  // Write your code here.
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
